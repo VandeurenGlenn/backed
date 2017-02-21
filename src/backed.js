6ea39247840c834b32bf96099a0ebcdfa1d56d5d
Glenn Vandeuren <vandeurenglenn@gmail.com>
2017-02-21 23:52:34 +0100

    :metal: Add loadScript to super

--- src/backed.js
+++ src/backed.js
@@ -2,6 +2,7 @@
 import Utils from './utils';
 import fireEvent from './internals/fire-event.js';
 import toJsProp from './internals/to-js-prop.js';
+import loadScript from './internals/load-script.js';
 import PubSubLoader from './internals/pub-sub-loader.js';
 PubSubLoader();
 
@@ -24,6 +25,7 @@ export default _class => {
       super();
       this.fireEvent = fireEvent.bind(this);
       this.toJsProp = toJsProp.bind(this);
+      this.loadScript = loadScript.bind(this);
 
       Utils.handleProperties(this, _class.properties);
       Utils.handleObservers(this, _class.observers);
