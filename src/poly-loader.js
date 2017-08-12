4b135567683b58f4251839eeb29a5d3bd8a8743b
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-12 20:41:30 +0200

    :8ball: Don't polyfill HTMLImports

--- src/poly-loader.js
+++ src/poly-loader.js
@@ -1,6 +1,5 @@
 'use strict';
 import loadScript from './internals/load-script';
-const supportsHTMLImports = 'import' in document.createElement('link');
 const supportsCustomElementsV1 = 'customElements' in window;
 const supportsShadowDOMV1 = !!HTMLElement.prototype.attachShadow;
 
@@ -13,12 +12,6 @@ const bowerMinUrl = (name, main) => {
 
 export default () => {
   return new Promise((resolve, reject) => {
-    if (!supportsHTMLImports) {
-      promises.push(loadScript(bowerMinUrl('html-imports')))
-    }
-    if (!supportsCustomElementsV1) {
-      promises.push(loadScript(bowerMinUrl('custom-elements')))
-    }
     if (!supportsShadowDOMV1) {
       promises.push(loadScript(bowerMinUrl('shadydom')))
       promises.push(
