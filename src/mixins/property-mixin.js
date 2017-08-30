4926f1ee6a056f2c271d1b9f415cffd66c2c8dd3
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-30 22:11:16 +0200

    :metal: Move properties to connectedCallback

--- src/mixins/property-mixin.js
+++ src/mixins/property-mixin.js
@@ -6,28 +6,26 @@ const render = window.Backed.Renderer;
 // TODO: Create & add global observer
 export default base => {
   return class PropertyMixin extends base {
-    constructor(options) {
+    constructor(options = {}) {
       super(options);
-      if (options.properties) {
-        for (const entry of Object.entries(options.properties)) {
-          const {observer, reflect, renderer} = entry[1];
+      this.properties = options.properties;
+    }
+
+    connectedCallback() {
+      // test first
+      // if (this.async) {
+        // return new Promise((resolve, reject) {
+        //   this.defineProperty()
+        // });
+      // }
+      if (this.properties) {
+        for (const entry of Object.entries(this.properties)) {
+          const { observer, reflect, renderer } = entry[1];
           if (observer || reflect || renderer) {
             if (renderer && !render) {
               console.warn('Renderer undefined');
             }
-            // TODO: test first
-            // if (options.async) {
-            //   return new Promise((resolve, reject) => {
-            //     this.defineProperty(entry[0], entry[1]);
-            //     resolve();
-            //   });
-            // } else {
-            // check if attribute is defined and update property with it's value
-            // else fallback to it's default value (if any)
             this.defineProperty(entry[0], entry[1]);
-            this[entry[0]] = this.hasAttribute(entry[0]) ?
-              this.getAttribute(entry[0]) : entry[1].value;
-            // }
           }
         }
       }
@@ -38,7 +36,7 @@ export default base => {
      * @param {boolean} options.reflect when true, reflects value to attribute
      * @param {function} options.render callback function for renderer (example: usage with lit-html, {render: render(html, shadowRoot)})
      */
-    defineProperty(property = null, {strict = false, observer, reflect = false, renderer}) {
+    defineProperty(property = null, {strict = false, observer, reflect = false, renderer, value}) {
       Object.defineProperty(this, property, {
         set(value) {
           if (value === this[`___${property}`]) return;
@@ -65,6 +63,10 @@ export default base => {
         },
         configurable: strict ? false : true
       });
+      // check if attribute is defined and update property with it's value
+      // else fallback to it's default value (if any)
+      const attr = this.getAttribute(property);
+      this[property] = attr || this.hasAttribute(property) || value;
     }
   }
 }
