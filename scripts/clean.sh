53f7e920f38196b3dbdc67a792ab9cdece610c09
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-30 18:55:36 +0200

    :metal: Update release.sh & add clean.sh

---
+++ scripts/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo 'cleaning ...'
+
+rm -rf backed.js
+rm -rf backed.min.js
+rm -rf backed.js.map
+rm -rf poly-loader.js
+rm -rf poly-loader.min.js
+rm -rf poly-loader.js.map
+rm -rf mixins/**
