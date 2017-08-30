f177a14fc93c8e2c94e2f82dd7b40f02d17553b3
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-30 22:59:31 +0200

    :neutral_face: Fix gh-pages.sh

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -6,15 +6,16 @@ REF=$(git rev-parse --verify HEAD)
 MSG="Deployed to Github pages: ${REF}"
 
 mkdir -p .gh-pages-tmp
-cp demo .gh-pages-tmp/demo
-cp docs .gh-pages-tmp
+cp demo/* .gh-pages-tmp/demo -r
+cp docs/** .gh-pages-tmp -r
+# cp docs/**/** .gh-pages-tmp
 # cp docs .gh-pages-tmp
 # TODO: add docs/demo selector ...
 echo "reminder::add docs/demo selector"
 cd .gh-pages-tmp
 git init
 git add .
-git commit -m "${msg}"
+git commit -m "${MSG}"
 
-git push -f --quiet "https://github.com/vandeurenglenn/backed" $SOURCE_BRANCH:$TARGET_BRANCH > /dev/null 2>&1
+git push -f "https://github.com/vandeurenglenn/backed" $SOURCE_BRANCH:$TARGET_BRANCH > /dev/null 2>&1
 cd ../
