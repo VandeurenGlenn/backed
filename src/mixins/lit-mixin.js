a253ee1f27cc10b126820272192311e69a32c192
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-08-30 22:08:07 +0200

    :neutral_face: Fix lit-html not imported

--- src/mixins/lit-mixin.js
+++ src/mixins/lit-mixin.js
@@ -1,4 +1,4 @@
-import { html, render } from '/node_modules/lit-html/lit-html.js';
+import { html, render } from './../../node_modules/lit-html/lit-html.js';
 
 window.html = window.html || html;
 window.Backed = window.Backed || {};
