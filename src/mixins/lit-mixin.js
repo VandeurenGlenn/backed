ddfd13148d127e25e0d5378e5f73ca343c3d474d
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-10-06 01:49:33 +0200

    :metal: Make lit-mixin smaller

--- src/mixins/lit-mixin.js
+++ src/mixins/lit-mixin.js
@@ -1,7 +1,5 @@
 import { html, render } from './../../node_modules/lit-html/lit-html.js';
-import { repeat } from './../../node_modules/lit-html/lib/repeat.js';
 window.html = window.html || html;
-window.repeat = window.repeat || repeat;
 window.Backed = window.Backed || {};
 window.Backed.Renderer = window.Backed.Renderer || render;
 
@@ -11,25 +9,25 @@ window.Backed.Renderer = window.Backed.Renderer || render;
  * @param {class} base class to extend from
  */
 export default base => {
- return class LitMixin extends base {
-   get propertyStore() {
-     return window.Backed.PropertyStore;
-   }
-   constructor(options = {}) {
-     super(options);
-     if (!this.shadowRoot) this.attachShadow({mode: 'open'});
-     if (!this._isValidRenderer(this.render)) throw 'Invalid renderer!'
-   }
-   connectedCallback() {
-     if (super.connectedCallback) super.connectedCallback();
-     if (this.render) render(this.render(), this.shadowRoot);
-     else throw 'Missing render method!';
-   }
-   _isValidRenderer(renderer) {
-     if (!renderer) {
+  return class LitMixin extends base {
+    constructor(options = {}) {
+      super(options);
+      if (!this.shadowRoot) this.attachShadow({mode: 'open'});
+      this._isValidRenderer(this.render);
+    }
+    connectedCallback() {
+      if (super.connectedCallback) super.connectedCallback();
+      if (this.render) render(this.render(), this.shadowRoot);
+    }
+    _isValidRenderer(renderer) {
+      if (!renderer) {
+       throw 'Missing render method!';
        return;
-     }
-     return String(renderer).includes('return html`') || String(renderer).includes('return this.template')
-   }
- }
+      }
+      if (!String(renderer).includes('return html`') &&
+         !String(renderer).includes('template')) {
+         throw 'Invalid renderer!';
+      }
+    }
+  }
 }
