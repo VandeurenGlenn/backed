5912cf8d1c395c4c7a310bc6339102c8d5e186e9
VandeurenGlenn <vandeurenglenn@gmail.com>
2017-10-08 16:39:02 +0200

    :metal: Cleanup code, gh-pages & dependencies

--- src/backed-element.js
+++ src/backed-element.js
@@ -1,7 +1,6 @@
 import LitMixin from './mixins/lit-mixin.js';
 import PropertyMixin from './mixins/property-mixin.js';
 import {define} from './utils.js';
-import { repeat } from './../node_modules/lit-html/lib/repeat.js';
 
 window.repeat = window.repeat || repeat;
 window.define = window.define || define;
