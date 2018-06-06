6449290fe2262bed89ad51206a9713b40671e200
vandeurenglenn <vandeurenglenn@gmail.com>
2018-06-07 01:17:33 +0200

    :metro: Add --css-elevation

--- src/mixins/css-mixin.js
+++ src/mixins/css-mixin.js
@@ -43,7 +43,40 @@
         top: 50%;
         transform: translate(-50%, -50%);
         border-radius: 2px;
-  `
+  `,
+  'mixin(--css-elevation-2dp)': `
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+                0 1px 5px 0 rgba(0, 0, 0, 0.12),
+                0 3px 1px -2px rgba(0, 0, 0, 0.2);`,
+
+  'mixin(--css-elevation-3dp)': `
+    box-shadow: 0 3px 4px 0 rgba(0, 0, 0, 0.14),
+                0 1px 8px 0 rgba(0, 0, 0, 0.12),
+                0 3px 3px -2px rgba(0, 0, 0, 0.4);`,
+  'mixin(--css-elevation-4dp)': `
+    box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+                0 1px 10px 0 rgba(0, 0, 0, 0.12),
+                0 2px 4px -1px rgba(0, 0, 0, 0.4);`,
+  'mixin(--css-elevation-6dp)': `
+    box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14),
+                0 1px 18px 0 rgba(0, 0, 0, 0.12),
+                0 3px 5px -1px rgba(0, 0, 0, 0.4);`,
+  'mixin(--css-elevation-8dp)': `
+    box-shadow: 0 8px 10px 1px rgba(0, 0, 0, 0.14),
+                0 3px 14px 2px rgba(0, 0, 0, 0.12),
+                0 5px 5px -3px rgba(0, 0, 0, 0.4);`,
+  'mixin(--css-elevation-12dp)': `
+    box-shadow: 0 12px 16px 1px rgba(0, 0, 0, 0.14),
+                0 4px 22px 3px rgba(0, 0, 0, 0.12),
+                0 6px 7px -4px rgba(0, 0, 0, 0.4);`,
+  'mixin(--css-elevation-16dp)': `
+    box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14),
+                0  6px 30px 5px rgba(0, 0, 0, 0.12),
+                0  8px 10px -5px rgba(0, 0, 0, 0.4);`,
+  'mixin(--css-elevation-24dp)': `
+    box-shadow: 0 24px 38px 3px rgba(0, 0, 0, 0.14),
+                0 9px 46px 8px rgba(0, 0, 0, 0.12),
+                0 11px 15px -7px rgba(0, 0, 0, 0.4);`
  };
 
  const classes = {
@@ -82,6 +115,30 @@
    }`,
    'apply(--css-hero)': `.hero {
       mixin(--css-hero)
+   }`,
+   'apply(--css-elevation-2dp)': `.elevation-2dp {
+      mixin(--css-elevation-2dp)
+   }`,
+   'apply(--css-elevation-3dp)': `.elevation-3dp {
+      mixin(--css-elevation-3dp)
+   }`,
+   'apply(--css-elevation-4dp)': `.elevation-4dp {
+      mixin(--css-elevation-4dp)
+   }`,
+   'apply(--css-elevation-6dp)': `.elevation-6dp {
+      mixin(--css-elevation-6dp)
+   }`,
+   'apply(--css-elevation-8dp)': `.elevation-8dp {
+      mixin(--css-elevation-8dp)
+   }`,
+   'apply(--css-elevation-12dp)': `.elevation-12dp {
+      mixin(--css-elevation-12dp)
+   }`,
+   'apply(--css-elevation-16dp)': `.elevation-16dp {
+      mixin(--css-elevation-16dp)
+   }`,
+   'apply(--css-elevation-18dp)': `.elevation-18dp {
+      mixin(--css-elevation-18dp)
    }`
  }
 export default base => {
@@ -112,16 +169,18 @@ export default base => {
             });
         }
       }
-      const style = this.shadowRoot.querySelector('style');
+      const styles = this.shadowRoot ? this.shadowRoot.querySelectorAll('style') : this.querySelectorAll('style');
       // const matches = style.innerHTML.match(/apply((.*))/g);
-      this._applyClasses(style.innerHTML).then(innerHTML => {
-        if (innerHTML) this.style.innerHTML = innerHTML;
-        this._applyMixins(style.innerHTML).then(innerHTML => {
+      styles.forEach(style => {
+        this._applyClasses(style.innerHTML).then(innerHTML => {
           if (innerHTML) this.style.innerHTML = innerHTML;
-        })
-      }).catch(error => {
-        console.error(error);
-      });
+          this._applyMixins(style.innerHTML).then(innerHTML => {
+            if (innerHTML) this.style.innerHTML = innerHTML;
+          })
+        }).catch(error => {
+          console.error(error);
+        });
+      })
       // this._applyVariables(matches, style);
     }
 
